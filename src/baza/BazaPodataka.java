c65b03fa1c3084a8fff9e72e389ee1d231efa6aa
Stefan <stefd996@gmail.com>
2017-05-22 17:38:22 +0200

    db file

---
+++ src/baza/BazaPodataka.java
@@ -0,0 +1,45 @@
+package baza;
+
+import java.sql.*;
+
+public class BazaPodataka {
+    private Connection conn;
+    private static BazaPodataka instanca;
+
+    //Singleton - promenljiva tipa Singleton mora biti static!
+    private BazaPodataka() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:knjige.db");
+        } catch ( Exception e ) {
+            System.err.println("Doslo je do greske pri konekciji na bazu podataka"
+                    + e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static BazaPodataka getInstanca() {
+        if(instanca == null)
+            instanca = new BazaPodataka();
+        return instanca;
+    }
+
+    public void automatskaTransakcija(boolean on_off) throws SQLException {
+        conn.setAutoCommit(on_off);
+    }
+
+    public void snimiTransakciju() throws SQLException { conn.commit(); }
+
+    //metoda za upite koji su tipa INSERT, UPDATE, DELETE
+    public int iudUpit(String sql) throws SQLException {
+        System.out.println(sql);
+        Statement statement = conn.createStatement();
+        return statement.executeUpdate(sql);
+    }
+
+    public ResultSet selectUpit(String sql) throws SQLException {
+        System.out.println(sql);
+        Statement statement= conn.createStatement();
+        return statement.executeQuery(sql);
+    }
+}
